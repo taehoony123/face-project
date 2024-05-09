dddb7e3f7a46387b1323f3b8c453eadca9626835
Taehoony <164149952+taehoony123@users.noreply.github.com>
2024-05-09 10:12:35 +0900

    Update script.js

--- Face-Recognition-JavaScript/script.js
+++ Face-Recognition-JavaScript/script.js
@@ -40,7 +40,7 @@ function loadLabeledImages() {
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
-      for (let i = 1; i <= 10; i++) {
+      for (let i = 1; i <= 5; i++) {
         const img = await faceapi.fetchImage(`labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
